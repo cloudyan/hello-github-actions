598e5d8c84d436b3b2107e78314adab731579183
cloudyan <1395093509@qq.com>
2023-09-26 11:22:27 +0800

    ci: test:ci add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom',
+    coverage: {
+      // provider: 'istanbul', // or 默认 'v8'
+      // 输出 coverage/
+      // reporter: ['text', 'html', 'clover', 'json'], // 默认
+      // reporter: [
+      //   ["lcov", { projectRoot: "./src" }],
+      //   ["json", { file: "coverage.json" }],
+      //   ["html"],
+      //   ["text"],
+      // ]
+    },
   },
 })
